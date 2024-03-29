bce6ea935e970b99f021ec9968df560a690f027c
manzhaimaksim <maxbbb88@gmail.com>
2024-03-29 13:07:41 +0300

    adding method form_for to HexletCode

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,4 +5,12 @@ require_relative "hexlet_code/version"
 # module HexletCode for generating tags
 module HexletCode
   autoload(:Tag, "./lib/tag.rb")
+
+  def self.form_for(*args)
+    if args[1].nil?
+      "<form action=\"#\" method=\"post\"></form>"
+    else
+      "<form action=\"#{args[1][:url]}\" method=\"post\"></form>"
+    end
+  end
 end
