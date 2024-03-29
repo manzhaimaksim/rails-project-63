66400b9e5612dcd54a1ad031dbe2bc35fb81ef96
manzhaimaksim <maxbbb88@gmail.com>
2024-03-29 13:07:21 +0300

    adding tests for HexletCode

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,11 +3,19 @@
 require "test_helper"
 
 class TestHexletCode < Minitest::Test
+  def setup
+    @user = Struct.new("User", :name, :job, keyword_init: true).new name: "rob"
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_it_does_something_useful
-    assert true
+  def test_that_form_is_created_without_url
+    assert HexletCode.form_for @user == "<form action=\"#\" method=\"post\"></form>"
+  end
+
+  def test_that_form_is_created_with_url
+    assert (HexletCode.form_for @user, url: "/users") == "<form action=\"/users\" method=\"post\"></form>"
   end
 end
