8ce6ff64c547c6baa1ba2f0ac48f49eb000b5d79
manzhaimaksim <maxbbb88@gmail.com>
2024-06-02 18:29:17 +0300

    editing module Tag

--- lib/tag.rb
+++ lib/tag.rb
@@ -1,29 +1,14 @@
 # frozen_string_literal: true
 
-# Tag for generating tags
+# module Tag for generating tags
 module HexletCode
   module Tag
-    def self.build(*args)
-      case args[0]
-      when "br"
-        "<br>"
-      when "img", "input"
-        ["<#{args[0]}", "#{convert_hash_to_string(args[1])}>"].join
-      when "label"
-        if args[1].nil?
-          ["<label>", yield, "</label>"].join
-        else
-          ["<label", "#{convert_hash_to_string(args[1])}>", "#{yield}</label>"].join
-        end
-      when "div"
-        "<div></div>"
-      else
-        "No such tag exists"
-      end
-    end
-
-    def self.convert_hash_to_string(hash)
-      hash.map { |key, value| [" #{key}=", "\"#{value}\""] }.to_a.join
+    def self.build(tag, attributes = {})
+      attrs = attributes.map do |key, value|
+        "#{key}=\"#{value}\""
+      end.join(' ')
+      attrs = attrs.empty? ? '' : " #{attrs}"
+      "<#{tag}#{attrs}#{block_given? ? ">#{yield}</#{tag}>" : ' />'}"
     end
   end
 end
