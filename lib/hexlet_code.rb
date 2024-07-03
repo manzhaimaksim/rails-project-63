7724d9a52af8631e9f8a24cf9b1cd91e4851f4dd
manzhaimaksim <maxbbb88@gmail.com>
2024-07-03 16:53:28 +0300

    editingmodule HexletCode

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,19 +1,8 @@
 # frozen_string_literal: true
 
-require_relative 'hexlet_code/version'
+require './lib/hexlet_code/version'
 
-# module HexletCode for generating tags
 module HexletCode
-  autoload :Tag, 'tag.rb'
-  autoload :FormContent, 'form_content.rb'
-
-  def self.form_for(fields_data = {}, attributes = {})
-    url = (attributes.is_a?(Hash) ? attributes.fetch(:url, '#') : attributes)
-    attrs = { action: url, method: 'post' }
-    attrs.merge!(attributes) if attributes.is_a?(Hash)
-    attrs.delete(:url)
-    content = FormContent.new(fields_data, Tag)
-    yield(content) if block_given?
-    Tag.build('form', attrs) { content if block_given? }
-  end
+  extend HexletCode
+  autoload :Tag, './lib/tag.rb'
 end
