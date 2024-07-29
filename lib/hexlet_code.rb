edcaf3a99c90de276697da4479dfda6a5dac6ccc
manzhaimaksim <maxbbb88@gmail.com>
2024-07-29 18:55:02 +0300

    refactoring module HexletCode

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -3,6 +3,15 @@
 require './lib/hexlet_code/version'
 
 module HexletCode
-  extend HexletCode
-  autoload :Tag, './lib/tag.rb'
+  autoload :Tag, 'hexlet_code/tag.rb'
+  autoload :Form, 'hexlet_code/form.rb'
+
+  def self.form_for(record, options = {})
+    default_options = { action: options.fetch(:url, '#'), method: 'post' }
+
+    form = Form.new(record)
+    yield(form) if block_given?
+
+    Tag.build('form', default_options.merge(options).except(:url)) { form.inputs.join('') }
+  end
 end
